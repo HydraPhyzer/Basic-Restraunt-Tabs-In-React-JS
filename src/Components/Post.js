0c983fba877739159b435cf243ba75399ca23d22
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-08 17:47:40 +0500

    App Ready to Use

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -5,17 +5,18 @@ const Post = (Props) => {
     return (
         <div className='Post'>
             {
-                Props.Posts.map(element => {
+                Props.Posts.map((element,key) => {
                     return(
-                        <div className="Post-Body">
+                        <div className="Post-Body" key={key}>
                             <div className="Head">
                                 <span className='Num'>{element.id}</span>
                                 <span className='Name'>{element.name}</span>
                                 <span className='Cat'>{element.category}</span>
                             </div>
                             <div className="Image">
-                                <img src={element.image} alt="" />
+                                <img src={element.image} alt=""/>
                             </div>
+                            
                             <div className="Desc">
                                 <p>{element.description}</p>
                             </div>
